07c3009611ff56e2d12e1f60c5b33a51459a70ab
Victor Tran <vicr12345@gmail.com>
2021-02-26 01:00:49 +1100

    Update trigger script

--- libthefrisbee/trigger-uevent.sh
+++ libthefrisbee/trigger-uevent.sh
@@ -6,6 +6,11 @@ if [ $EUID -ne 0 ]; then
 fi;
 if [ $# -eq 0 ]; then
     echo "Usage: $0 [device]"
+    exit 1;
+fi;
+if ! [ -b $1 ]; then
+    echo "error: $1 is not a block device"
+    exit 1;
 fi;
 
 udevadm trigger $1
