afed3a36d8b54fe595f5d2cb8ae5fe52111bc3e3
Victor Tran <vicr12345@gmail.com>
2022-01-20 20:51:38 +0000

    Added translation using Weblate (Vietnamese)

---
+++ application/translations/vi.ts
@@ -0,0 +1,334 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="vi">
+<context>
+    <name>CreateDiskImagePopover</name>
+    <message>
+        <location filename="../operations/creatediskimagepopover.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../operations/creatediskimagepopover.ui" line="35"/>
+        <location filename="../operations/creatediskimagepopover.ui" line="52"/>
+        <source>Create Disk Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../operations/creatediskimagepopover.ui" line="45"/>
+        <source>Browse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../operations/creatediskimagepopover.ui" line="72"/>
+        <source>DISK IMAGE OPTIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../operations/creatediskimagepopover.ui" line="82"/>
+        <source>Disk Image Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../operations/creatediskimagepopover.ui" line="89"/>
+        <source>Output File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../operations/creatediskimagepopover.ui" line="96"/>
+        <source>Attach this disk image</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DiskPane</name>
+    <message>
+        <location filename="../diskpane.ui" line="35"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.ui" line="82"/>
+        <source>JOBS RUNNING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.ui" line="89"/>
+        <source>Some tasks may be unavailable while jobs are currently being run on this item.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.ui" line="114"/>
+        <source>View Jobs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.ui" line="232"/>
+        <source>Check</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.ui" line="239"/>
+        <source>Edit Partitions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.ui" line="246"/>
+        <source>Image</source>
+        <extracomment>A verb: &quot;Create Image&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.ui" line="253"/>
+        <source>Restore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.ui" line="273"/>
+        <source>Erase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.cpp" line="130"/>
+        <source>Check this disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.cpp" line="131"/>
+        <source>Check this disk for errors?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskpane.cpp" line="132"/>
+        <source>Depending on the size of the disk, the check may take a while.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>theFrisbee</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="137"/>
+        <source>Disks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="180"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="183"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="192"/>
+        <source>Attach Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="200"/>
+        <source>Create Disk Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="203"/>
+        <source>Ctrl+N</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OverviewDiskPane</name>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.ui" line="20"/>
+        <source>Icon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.ui" line="40"/>
+        <location filename="../diskPanes/overviewdiskpane.ui" line="50"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.ui" line="64"/>
+        <source>Detach</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.ui" line="75"/>
+        <source>Unmount</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.ui" line="86"/>
+        <source>Mount</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.ui" line="97"/>
+        <source>Eject</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="75"/>
+        <source>Blank Disc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="80"/>
+        <source>CD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="83"/>
+        <source>CD-R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="86"/>
+        <source>CD-RW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="89"/>
+        <source>DVD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="92"/>
+        <source>DVD-R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="95"/>
+        <source>DVD-RW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="98"/>
+        <source>DVD-RAM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="101"/>
+        <source>DVD+R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="104"/>
+        <source>DVD+RW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="107"/>
+        <source>DVD+R Dual Layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="110"/>
+        <source>DVD+RW Dual Layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="113"/>
+        <source>BD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="116"/>
+        <source>BD-R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="119"/>
+        <source>BD-RE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="122"/>
+        <source>HD-DVD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="125"/>
+        <source>HD-DVD-R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="128"/>
+        <source>HD-DVD-RW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="131"/>
+        <source>Magneto Optical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../diskPanes/overviewdiskpane.cpp" line="137"/>
+        <source>No Disc Inserted</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="41"/>
+        <location filename="../main.cpp" line="74"/>
+        <source>Disk Utility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="77"/>
+        <source>The operation to perform. Valid operations:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="78"/>
+        <source>The device to perform the operation on</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="90"/>
+        <source>invalid operation %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="91"/>
+        <location filename="../main.cpp" line="99"/>
+        <location filename="../main.cpp" line="107"/>
+        <location filename="../main.cpp" line="120"/>
+        <source>Usage: %1 [options] [operation device].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="92"/>
+        <location filename="../main.cpp" line="100"/>
+        <location filename="../main.cpp" line="108"/>
+        <location filename="../main.cpp" line="121"/>
+        <source>%1 -h for more information.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="98"/>
+        <source>missing device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="106"/>
+        <source>too many arguments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="119"/>
+        <source>invalid device</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
