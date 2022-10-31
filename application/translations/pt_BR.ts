681ab485cc283cd784ace4f938c8989775867ef0
Parlance <parlance@vicr123.com>
2022-10-31 22:37:58 +0000

    Update Translations

---
+++ application/translations/pt_BR.ts
@@ -0,0 +1,344 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1">
+  <context>
+    <name>CreateDiskImagePopover</name>
+    <message>
+      <translation></translation>
+      <location filename="../operations/creatediskimagepopover.ui" line="35" />
+      <location filename="../operations/creatediskimagepopover.ui" line="52" />
+      <source>Create Disk Image</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../operations/creatediskimagepopover.ui" line="45" />
+      <source>Browse...</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../operations/creatediskimagepopover.ui" line="71" />
+      <source>DISK IMAGE OPTIONS</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../operations/creatediskimagepopover.ui" line="81" />
+      <source>Disk Image Size</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../operations/creatediskimagepopover.ui" line="88" />
+      <source>Output File</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../operations/creatediskimagepopover.ui" line="95" />
+      <source>Attach this disk image</source>
+    </message>
+  </context>
+  <context>
+    <name>DiskPane</name>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="35" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="219" />
+      <source>Title</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="82" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="220" />
+      <source>JOBS RUNNING</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="89" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="221" />
+      <source>Some tasks may be unavailable while jobs are currently being run on this item.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="114" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="222" />
+      <source>View Jobs</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="232" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="223" />
+      <source>Check</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="239" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="224" />
+      <source>Edit Partitions</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="246" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="225" />
+      <source>Image</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="253" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="226" />
+      <source>Restore</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.ui" line="273" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_diskpane.h" line="227" />
+      <source>Erase</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.cpp" line="130" />
+      <source>Check this disk</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.cpp" line="131" />
+      <source>Check this disk for errors?</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskpane.cpp" line="132" />
+      <source>Depending on the size of the disk, the check may take a while.</source>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="14" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="208" />
+      <source>theFrisbee</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="137" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="219" />
+      <source>Disks</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="185" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="220" />
+      <source>File</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="191" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="221" />
+      <source>Image</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="205" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="209" />
+      <source>Exit</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="208" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="211" />
+      <source>Ctrl+Q</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="217" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="213" />
+      <source>Attach Image</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="226" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="214" />
+      <source>Create Disk Image</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="229" />
+      <location filename="../../../build-theFrisbee-Chroot_6-Debug/application/thefrisbee_autogen/include/ui_mainwindow.h" line="216" />
+      <source>Ctrl+N</source>
+    </message>
+  </context>
+  <context>
+    <name>OverviewDiskPane</name>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.ui" line="64" />
+      <source>Detach</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.ui" line="75" />
+      <source>Unmount</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.ui" line="86" />
+      <source>Mount</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.ui" line="97" />
+      <source>Eject</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="75" />
+      <source>Blank Disc</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="80" />
+      <source>CD</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="83" />
+      <source>CD-R</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="86" />
+      <source>CD-RW</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="89" />
+      <source>DVD</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="92" />
+      <source>DVD-R</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="95" />
+      <source>DVD-RW</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="98" />
+      <source>DVD-RAM</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="101" />
+      <source>DVD+R</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="104" />
+      <source>DVD+RW</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="107" />
+      <source>DVD+R Dual Layer</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="110" />
+      <source>DVD+RW Dual Layer</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="113" />
+      <source>BD</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="116" />
+      <source>BD-R</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="119" />
+      <source>BD-RE</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="122" />
+      <source>HD-DVD</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="125" />
+      <source>HD-DVD-R</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="128" />
+      <source>HD-DVD-RW</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="131" />
+      <source>Magneto Optical</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../diskPanes/overviewdiskpane.cpp" line="137" />
+      <source>No Disc Inserted</source>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="43" />
+      <location filename="../main.cpp" line="65" />
+      <source>Disk Utility</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="68" />
+      <source>The operation to perform. Valid operations:</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="69" />
+      <source>The device to perform the operation on</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="81" />
+      <source>invalid operation %1</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="82" />
+      <location filename="../main.cpp" line="90" />
+      <location filename="../main.cpp" line="98" />
+      <location filename="../main.cpp" line="111" />
+      <source>Usage: %1 [options] [operation device].</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="83" />
+      <location filename="../main.cpp" line="91" />
+      <location filename="../main.cpp" line="99" />
+      <location filename="../main.cpp" line="112" />
+      <source>%1 -h for more information.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="89" />
+      <source>missing device</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="97" />
+      <source>too many arguments</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="110" />
+      <source>invalid device</source>
+    </message>
+  </context>
+</TS>
