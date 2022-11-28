37cbcf6e2515dcf0fa9ef2cb2d3902b4cf31a821
Parlance <parlance@vicr123.com>
2022-11-28 11:03:46 +0000

    Update Translations - vi, he_IL
    Co-Authored-By: Victor Tran <vicr12345@gmail.com>

--- application/translations/he_IL.ts
+++ application/translations/he_IL.ts
@@ -1,5 +1,5 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
-<TS version="2.1">
+<TS version="2.1" language="he_IL">
   <context>
     <name>CreateDiskImagePopover</name>
     <message>
--- libthefrisbee/translations/vi.ts
+++ libthefrisbee/translations/vi.ts
@@ -1,5 +1,5 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
-<TS version="2.1">
+<TS version="2.1" language="vi">
   <context>
     <name>DiskObject</name>
     <message>
